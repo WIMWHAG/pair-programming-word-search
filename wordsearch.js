28cee6272aa064ce574992e4a5ef5c071f37adca
WIMWHAG <william.johnrivera@yahoo.com>
2022-07-07 03:34:24 +0000

    worsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,19 +1,10 @@
 const wordSearch = (letters, word) => {
-    if (letters.length < 1 || word.length < 1) return false;
-    const horizontalJoin = letters.map((ls) => ls.join(""));
-    const verticalJoin = letters[0].map((col, index) =>
-      letters.map((row) => row[index])
-    );
-    const verticalFlip = verticalJoin.map((ls) => ls.join(""));
-    for (l of horizontalJoin) {
-      if (l.includes(word)) return true;
-      else {
-        for (l of verticalFlip) {
-          if (l.includes(word)) return true;
-        }
-      }
-    }
-    return false;
-  };
-  
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+
+};
+
+module.exports = wordSearch;
